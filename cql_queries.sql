281eac408ed20ace5869ef09cd214e831fc72eb4
Abel Gustavo Conde <conde@intralot.com>
2016-08-05 17:19:43 -0300

    added

--- cql_queries.sql
+++ cql_queries.sql
@@ -12,3 +12,35 @@ drop index if exists auditing.role1_index;
 
 create index if exists date1_index on auditing.user_configuration_change (date);
 
+select * from auditing.user_configuration_change
+where
+date in ('2016.07.01','2016.07.02','2016.07.03','2016.07.04','2016.07.05','2016.07.06','2016.07.07','2016.07.08','2016.07.09','2016.07.10','2016.07.11','2016.07.12','2016.07.13','2016.07.14','2016.07.15','2016.07.16','2016.07.17','2016.07.18','2016.07.19','2016.07.20','2016.07.21','2016.07.22','2016.07.23','2016.07.24','2016.07.25','2016.07.26','2016.07.27','2016.07.28','2016.07.29','2016.07.30','2016.07.31') 
+and hour in (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23) 
+and auditlevel in ('Low', 'Medium', 'High') limit 100 allow filtering;
+
+find . -name '*.qml' -print | grep -i '' | less
+
+
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+cont=0
+for f in $(find . chile -name '*.*')
+do
+	(( cont += 1 ))
+	datetime=$(stat -c %y "$f")
+		
+	date=$(echo "$datetime"| cut -c1-10)
+	hour=$(echo "$datetime"| cut -c12-13)
+	minute=$(echo "$datetime"| cut -c15-16)
+	
+	hourvalue=$(echo "14")
+	minutevalue=$(echo "45")
+
+	if [[ ( $date == "2016-08-03" ) ]] && (( hour >= hourvalue )) && (( minute >= minutevalue ))
+	then
+		echo "$f"
+	fi
+done
+IFS=$SAVEIFS 
+
