2b421aedc3a9ecc06c592055ece77f085192aa03
Abel Gustavo Conde <conde@intralot.com>
2016-08-08 16:45:42 -0300

    added

--- sample.sh
+++ sample.sh
@@ -2,7 +2,7 @@
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 cont=0
-for f in $(find . test -name '*.*' -type f)
+for f in $(find . ./chile -type f \( -name '*.h' -or -name '*.c' -or -name '*.cpp' -or -name '*.html' -or -name '*.htm' -or -name '*.qml' -or -name '*.js' -or -name '*.json' -or -name '*.txt' -or -name '*.css' \))
 do
 	(( cont += 1 ))
 	datetime=$(stat -c %y "$f")
@@ -10,16 +10,16 @@ do
 	date=$(echo "$datetime"| cut -c1-10 | sed 's/^0*//')
 	hour=$(echo "$datetime"| cut -c12-13 | sed 's/^0*//')
 	minute=$(echo "$datetime"| cut -c15-16 | sed 's/^0*//')
-	second=$(echo "$datetime"| cut -c18-19 | sed 's/^0*//')
 	
-	hourvalue=$(echo "17")
-	minutevalue=$(echo "22")
-	secondvalue=$(echo "0")
+	hourvalue=$(echo "14")
+	minutevalue=$(echo "45")
 
-	if [[ ( $date == "2016-08-05" ) ]] && (( hour >= hourvalue )) && (( minute >= minutevalue )) && (( second >= secondvalue ))
+	hourvalue2=$(echo "15")
+	minutevalue2=$(echo "0")
+
+	if [[ ( $date == "2016-08-03" ) ]] && ( ( (( hour >= hourvalue )) && (( minute >= minutevalue )) ) || ( (( hour >= hourvalue2 )) && (( minute >= minutevalue2 )) ) )
 	then
 		echo "$f"
 	fi	
-
 done
 IFS=$SAVEIFS 
