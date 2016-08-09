4a51c4cd8e08063e56054025c778440327d24c2f
Abel Conde <abel@gmail.com>
2016-08-09 07:45:45 -0300

    removed

--- create_index.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX IF NOT EXISTS remarks_index ON auditing.system_configuration_change (remarks);
-SELECT * FROM auditing.system_configuration_change WHERE remarks = 'logged in user: AlertUser';
-DROP INDEX auditing.remarks_index;
