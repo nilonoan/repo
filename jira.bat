111305dd223973847db403f57229940207dfa169
Abel Gustavo Conde <conde@intralot.com>
2016-08-11 08:27:34 -0300

    added

--- jira.bat
+++ jira.bat
@@ -5,11 +5,12 @@ SET USER=conde
 SET PASSWORD=123456q@
 
 REM We set the time when we want to trigger this script
-SET HOUR=0
-SET MINUTE=5
+SET HOUR=8
+SET MINUTE=22
 
-REM We set the navigator application name to be used to open the log-in we page
-SET APP_NAVIGATOR=firefox
+REM We set the navigator application name to be used to open the log-in web page
+REM Eihter "firefox" or "chrome" value are possible
+SET APP_NAVIGATOR=chrome
 
 REM We call javascript file
 cscript jira.js "%USER%" "%PASSWORD%" "%HOUR%" "%MINUTE%" "%APP_NAVIGATOR%"
